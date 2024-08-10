5b945051ddf2e2119e75e6a03894fa8a04a7c985
001-mak <ahsankhaleeq10@gmail.com>
2024-08-11 02:05:24 +0500

    checkout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,32 @@
+'use client'
+import {Elements} from "@stripe/react-stripe-js"
+import {loadStripe} from "@stripe/stripe-js"
+import CheckoutPage from "./components/CheckoutPage";
+import convertToSubcurrency from "./lib/convertToSuburrency";
 
+const stripePromise = loadStripe(process.env.STRIPE_PUBLIC_KEY!)
 export default function Home() {
+  const amount = 45.00;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello</h1>
+    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
+      <div className="mb-10">
+        <h1 className="text-4xl font-extrabold mb-2">Sonny</h1>
+        <h2 className="text-2xl">
+          has requested
+          <span className="font-bold"> ${amount}</span>
+        </h2>
+      </div>
+
+      <Elements
+        stripe={stripePromise}
+        options={{
+          mode: "payment",
+          amount: convertToSubcurrency(amount),
+          currency: "usd",
+        }}
+      >
+        <CheckoutPage amount={amount} />
+      </Elements>
     </main>
   );
 }
