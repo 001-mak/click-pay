2e186de7af470fb929aa2f08ff12dab3aade463c
001-mak <ahsankhaleeq10@gmail.com>
2024-08-11 12:55:00 +0500

    stripe payment success

--- src/app/components/CheckoutPage.tsx
+++ src/app/components/CheckoutPage.tsx
@@ -63,37 +63,35 @@ const CheckoutPage = ({ amount }: { amount: number }) => {
     setLoading(false);
   };
 
-//   if (!clientSecret || !stripe || !elements) {
+  if (!clientSecret || !stripe || !elements) {
     return (
-        <>
-        <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center">
         <div
-        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
           role="status"
-          >
+        >
           <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
             Loading...
-            </span>
-            </div>
-            </div>
-            </>
-        )
-//   }
+          </span>
+        </div>
+      </div>
+    );
+  }
 
-//   return (
-//     <form onSubmit={handleSubmit} className="bg-white p-2 rounded-md">
-//       {clientSecret && <PaymentElement />}
+  return (
+    <form onSubmit={handleSubmit} className="bg-white p-2 rounded-md">
+      {clientSecret && <PaymentElement />}
 
-//       {errorMessage && <div>{errorMessage}</div>}
+      {errorMessage && <div>{errorMessage}</div>}
 
-//       <button
-//         disabled={!stripe || loading}
-//         className="text-white w-full p-5 bg-black mt-2 rounded-md font-bold disabled:opacity-50 disabled:animate-pulse"
-//       >
-//         {!loading ? `Pay $${amount}` : "Processing..."}
-//       </button>
-//     </form>
-//   );
+      <button
+        disabled={!stripe || loading}
+        className="text-white w-full p-5 bg-black mt-2 rounded-md font-bold disabled:opacity-50 disabled:animate-pulse"
+      >
+        {!loading ? `Book Session` : "Processing..."}
+      </button>
+    </form>
+  );
 };
 
 export default CheckoutPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,18 @@ import {loadStripe} from "@stripe/stripe-js"
 import CheckoutPage from "./components/CheckoutPage";
 import convertToSubcurrency from "./lib/convertToSuburrency";
 
-const stripePromise = loadStripe(process.env.STRIPE_PUBLIC_KEY!)
+if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
+  throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined");
+}
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
 export default function Home() {
   const amount = 45.00;
   return (
     <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
       <div className="mb-10">
-        <h1 className="text-4xl font-extrabold mb-2">Sonny</h1>
+        <h1 className="text-4xl font-bold mb-2">Book 1:1 Session</h1>
         <h2 className="text-2xl">
-          has requested
+          1 hour at
           <span className="font-bold"> ${amount}</span>
         </h2>
       </div>
